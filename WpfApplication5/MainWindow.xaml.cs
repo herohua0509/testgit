c4d19e27dadc2c56a7bc8d7e98ef2afa2d5bce0a
herohua0509 <52254904@qq.com>
2016-03-30 15:05:07 +0800

    branch

--- WpfApplication5/MainWindow.xaml.cs
+++ WpfApplication5/MainWindow.xaml.cs
@@ -40,7 +40,7 @@ namespace WpfApplication5
 
                 IApplicationContext ctx = ContextRegistry.GetContext();
                 IObjectFactory factory = (IObjectFactory)ctx;
-                IModel m2 = (IModel)factory.GetObject("TestModel311111333111");
+                IModel m2 = (IModel)factory.GetObject("TestModel");
                
                 m2.Show();
             }
